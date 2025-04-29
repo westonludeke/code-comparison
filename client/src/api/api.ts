c123c694cd9a372c69217ed52489fdf2832a9b51
westonludeke <westonludeke@gmail.com>
2025-04-29 15:35:27 -0500

    update client/src/api/api.ts for api calls to always hit the deployment url

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -5,7 +5,7 @@ import axios, {
 } from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: import.meta.env.VITE_API_URL + '/api',
   headers: {
     'Content-Type': 'application/json',
   },
